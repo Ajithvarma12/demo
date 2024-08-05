1589a76ed0b2e0d7c776b54b54e0ada01669bcad
Ajithvarma12 <ajithk59342@gmail.com>
2024-08-05 09:41:54 +0530

    demogit2

---
+++ citizen.java
@@ -0,0 +1,31 @@
+class citizen
+{
+ public static void main(String args[])
+  {
+   int age = 50,graduation=2020;
+   if(age>18)
+    {
+     System.out.println("eligible");
+    }
+    else
+    {
+     System.out.println("not eligible");
+    }
+    if(graduation<2020)
+     {
+      System.out.println("graduation completed");
+     }
+     else
+     {
+      System.out.println("graduation not completed");
+     }
+     if(age>50)
+      {
+       System.out.println("senior citizen");
+      }
+      else
+      {
+       System.out.println("not senior citizen");
+      }
+  }
+}
---
+++ coding_hub.java
@@ -0,0 +1,13 @@
+class Hub
+{
+ public static void main(String args[])
+ {
+  int a=1;
+  while(a<=100)
+  {
+   System.out.println("java");
+   System.out.println("programming"); 
+   a++;
+  }
+ }
+}
